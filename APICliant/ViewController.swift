8e7a36971318b808bf990e771d5a222daa06766d
taiga <matsuotaiga.miraidenshi@gmail.com>
2020-09-23 09:37:00 +0900

    完成

--- APICliant/ViewController.swift
+++ APICliant/ViewController.swift
@@ -5,16 +5,84 @@
 //  Created by 松尾大雅 on 2020/09/21.
 //  Copyright © 2020 litech. All rights reserved.
 //
-
 import UIKit
+
+struct Article: Codable {
+        var title: String
+        var user: User
+struct User: Codable {
+        var id: String
+    }
+}
+
+struct CopiedQiita {
+
+    static func fetchArticle(completion: @escaping ([Article]) -> Swift.Void) {
+
+        let url = "https://qiita.com/api/v2/items"
+
+        guard var urlComponents = URLComponents(string: url) else {
+            return
+        }
+
+        urlComponents.queryItems = [
+            URLQueryItem(name: "per_page", value: "50"),
+        ]
+
+        let task = URLSession.shared.dataTask(with: urlComponents.url!) { data, response, error in
+
+            guard let jsonData = data else {
+                return
+            }
+
+            do {
+                let articles = try JSONDecoder().decode([Article].self, from: jsonData)
+                completion(articles)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        task.resume()
+    }
+}
+
 
 class ViewController: UIViewController {
 
+    private var tableView = UITableView()
+    fileprivate var articles: [Article] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "最新記事"
+
+        setUpTableView: do {
+            tableView.frame = view.frame
+            tableView.dataSource = self
+            view.addSubview(tableView)
+        }
+
+        CopiedQiita.fetchArticle(completion: { (articles) in
+            self.articles = articles
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        })
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        let article = articles[indexPath.row]
+        cell.textLabel?.text = article.title
+        cell.detailTextLabel?.text = article.user.id
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articles.count
+    }
+}
